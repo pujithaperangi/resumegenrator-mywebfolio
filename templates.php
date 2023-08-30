c9c9cc11e8bea9e8d6ff3bcfac2c4e39a43244ce
pujithaperangi <pujithaperangi@gmail.com>
2023-08-30 13:14:24 +0530

    temlates

---
+++ templates.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>templates</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+   
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+<style>
+   .card{
+    box-shadow:3px 3px 7px 9px rgba(0, 0, 0, 0.1);
+   }
+   body {
+            background-color: #9ec9db;
+        }
+</style>
+</head>
+<body >
+    <div class="container mt-5">
+        <h1 class="text-center">Templates</h1>
+        <br><br>
+    <div class="card-group">
+  <div class="card">
+    <img src="classic.png" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Classic</h5>
+      <p class="card-text">classic template for professionals vocations who are just beginners.</p>
+      <a type="button"class="btn btn-success" href="classic.php" ><i class="bi bi-file-earmark-plus-fill"></i>create</a>
+    </div>
+  </div>
+  <div class="card">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card 
+           
+        title</h5>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+</div>
+</div>
+    
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
