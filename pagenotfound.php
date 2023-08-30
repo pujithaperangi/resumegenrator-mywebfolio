76980995a291339e7d108b356d1a4b0d7c130c60
pujithaperangi <pujithaperangi@gmail.com>
2023-08-30 13:02:26 +0530

    pagenotfound

---
+++ pagenotfound.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+</head>
+<body>
+                    <div class="card my-3">
+                        <div class="card-body">
+                            <h1 class="text-center text-muted">page not found</h2>
+                        </div>
+                    </div>
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    
+</body>
+</html> 
